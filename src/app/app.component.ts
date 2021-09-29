7a0c7d69fa4247d1fffa08e2100c4c1b2670e895
Maciej Kędra <maciejken@gmail.com>
2021-09-29 07:09:55 +0200

    first commit with a bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chat-app';
+export class AppComponent implements OnInit {
+
+  @Input() message!: string;
+  @Output() messageChange = new EventEmitter<string>();
+  newMessage = '';
+  messageList: string[] = [];
+
+  constructor(private chatService: ChatService){
+
+  }
+
+  ngOnInit(){
+    this.chatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    })
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+  }
 }
